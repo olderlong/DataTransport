61370a0586f055b41cac641c686154500289a570
Antares <olderlong@163.com>
2018-06-14 11:38:21 +0800

    添加扫描结果接收与打印处理

--- server/cc_server.py
+++ server/cc_server.py
@@ -30,13 +30,35 @@ class CCServer(UDPEndPoint):
                 # print("收到代理{}的漏扫状态数据".format(address))
 
             elif pkg_obj["Type"] == "ScanResult":
-                agent_event.event_scan_result.dict = pkg_obj["Data"]
-                event_manager.send_event(agent_event.event_scan_result)
+                agent_event.event_scan_result_recv.dict = pkg_obj["Data"]
+                self.__print_scan_result(pkg_obj["Data"])
+                event_manager.send_event(agent_event.event_scan_result_recv)
+
                 # print("收到代理{}的漏扫结果数据".format(address))
             else:
                 print("收到来自{}的未知类型数据".format(address))
         except KeyError as e:
             print("收到来自{}的未知类型数据——{}".format(address, data))
+
+#######################################################################################################################
+    def __print_scan_result(self, vul_result):
+        result_str = "漏洞类型:\t{}\n漏洞URL:\t{}\n漏洞等级:\t{}\n漏洞信息:\t".format(
+            vul_result["VulType"],
+            vul_result["VulUrl"],
+            vul_result["VulSeverity"]
+        )
+        print(result_str)
+        for info in vul_result["VulDetails"]:
+            result_str = "\tURL参数变异:\t{}\n\t漏洞原因:\t{}\n\tCWE:\t{}\n\tCVE:\t{}".format(
+                info["url_param_variant"],
+                info["vul_reasoning"],
+                info["CWE"],
+                info["CVE"]
+            )
+            print(result_str)
+        print("\r\n")
+
+#######################################################################################################################
 
     def send_command(self, event):
         command_json = event.dict
